f8d48a8bc5165b1da73d32a79014914b0e54f41a
clayton wright <claytonwright@claytons-MacBook-Pro.local>
2022-10-27 10:08:19 -0400

    repo fixed

---
+++ fullstack-intro/src/Components/BlogList.js
@@ -0,0 +1,16 @@
+import BlogListCard from "./BlogListCard";
+
+
+const BlogList = ({ blogs }) => {
+	return (
+		<div className="blog-list">
+			{blogs.map((blog) => (
+				<div className="blog-preview" key={blog.id}>
+					<BlogListCard blog={blog} />
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default BlogList;
